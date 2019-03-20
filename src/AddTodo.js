6c617ed1893a3f3867463de975fb8b25be715905
Zach Dickens <dickens.52@buckeyemail.osu.edu>
2019-03-20 11:35:21 -0400

    Initial Commit

---
+++ src/AddTodo.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class AddTodo extends Component {
+  onAdd = () => {
+    this.props.setText("");
+  };
+  onTextChange = event => {
+    this.props.setText(event.target.value);
+  };
+  render() {
+    const { text } = this.props;
+    return (
+      <>
+        <label>
+          Item:
+          <input type="text" value={text} onChange={this.onTextChange} />
+        </label>
+        <button onClick={this.onAdd}>Add</button>
+      </>
+    );
+  }
+}
+
+export default AddTodo;
